2ef4bc26deb7699cbba416809e656b179c3c5c8b
Justin Boyson <unclejustin@gmail.com>
2014-11-21 11:15:05 -0600

    Preview layout complete.

--- ecosystem/job/directive/jobPreview/jobPreview.js
+++ ecosystem/job/directive/jobPreview/jobPreview.js
@@ -9,6 +9,52 @@ angular.module('orca').directive('jobPreview', function() {
 		link: function(scope, element, attrs, fn) {
 
 
+		},
+		controller:function($scope) {
+			$.getJSON("preview.json", function (json) {
+				$scope.preview = json;
+				$scope.$apply();
+			});
+
+			$scope.getChangeBg = function(change) {
+				var classes = [];
+
+				switch(change) {
+					case 'add':
+						classes.push('alert-success');
+						break;
+					case 'edit':
+						classes.push('alert-warning');
+						break;
+					case 'delete':
+						classes.push('alert-danger');
+						break;
+					default:
+						break;
+				}
+
+				return classes;
+			};
+
+			$scope.getChangeIcon = function(change) {
+				var classes = [];
+
+				switch(change) {
+					case 'add':
+						classes.push('fa-plus');
+						break;
+					case 'edit':
+						classes.push('fa-exclamation');
+						break;
+					case 'delete':
+						classes.push('fa-times');
+						break;
+					default:
+						break;
+				}
+
+				return classes;
+			};
 		}
 	};
 });
